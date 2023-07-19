41e54847dccdd3bad80612440b574bd9569db5b1
Sanjid <dsanjid@gmail.com>
2023-07-19 11:44:22 -0400

    made stepabl

--- practice-for-ch-ruby-chess-long-practice-main/stepable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/stepable.rb
@@ -1,6 +1,22 @@
 module Stepable
 
+    def moves 
+        moves = []
+        cur_x,cur_y = pos
+        move_dirs.each do |dx,dy|
+            poss_pos = [cur_x + dx, cur_y + dy]
+            if board.valid_pos?(poss_pos)
+                if board.empty?(poss_pos)
+                    moves << poss_pos
+                    
+                else
+                    moves << poss_pos if board[poss_pos].color != color
+                end
+            end
+        end
 
-    
+        moves
+    end
+            
 
 end
